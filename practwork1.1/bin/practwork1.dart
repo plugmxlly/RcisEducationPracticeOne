2745dd4ae66b1f0c9a71850e0ff4ce6eed5f237f
public <public@copp.local>
2022-10-31 12:03:53 +0700

    gg

---
+++ practwork1.1/bin/practwork1.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+import 'dart:io';
+
+
+void main() {
+// Задание 1
+
+// int minNumber = 999;          
+
+// int numbers;
+
+// List<int> randomNums = [];
+ 
+// Random random = new Random();
+
+// for (int i = 0; i <= 15; i++) {
+//   numbers = random.nextInt(99) - 1;
+
+//   randomNums.add(numbers) ;
+//   if (numbers < minNumber) {
+//     minNumber = numbers;
+//   }
+//   print("${numbers}") ;
+
+// }
+
+// print("Минимальное число - $minNumber");
+
+// Задание 2
+
+// double sum = 0;
+// double multiply = 1;
+// double count = 0;
+// double num = 1;
+
+// while (num != 0) {
+//     num = double.parse(stdin.readLineSync()!);
+//     sum += num;
+
+//     if (num != 0) {
+//       multiply *= num;
+//       count++;
+//     }
+//   }
+//   print("сумма - ${sum}");
+//   print("Произведение - ${multiply}");
+//   print("Среднее - ${sum / count}");
+
+// Задание 3
+
+// List<String> abc = [];
+
+// String words = "day";
+// int a = 0;
+// int b = 0;
+// int minLength = 0;
+// int maxLength = 0;
+// int i = 0;
+
+// while (words != "") {
+//   words = stdin.readLineSync()!;
+//   abc.add(words);
+//   if (words.length < minLength && words.length != 0) {
+//     minLength = words.length;
+//     a = i;
+//   }
+//   if (words.length > maxLength && words.length != 0) {
+//     maxLength = words.length;
+//     b = i;
+//   }
+//   i++;
+// }
+// print("Кратчайшее слово - ${abc[a]} \n Длиннейшее слово - ${abc[b]}");
+
+// Задание 4
+
+// List <int> nums = [];
+
+// print ("Введите начало диапазона ");
+// int a = int.parse(stdin.readLineSync()!);
+// print ("Введите начало диапазона ");
+// int b = int.parse(stdin.readLineSync()!);
+
+// Random random = new Random();
+
+// for (int i = 0; i < 10; i++) {
+//   nums.add(random.nextInt(b - a) + a);
+//  }
+
+// for (int i = 0; i < nums.length; i++) {
+//   stdout.write("${nums[i]} ");
+//  }
+
+// Задание 5
+
+// String abc = stdin.readLineSync()!;
+// List<String> words = abc.split(" ");
+
+// words.insert(0, "Start");
+// words.add ("End");
+
+// abc = words.join(" ");
+// print(words.length - 2);
+// print(abc);
+
+}
---
+++ practwork1.1/test/practwork1_test.dart
@@ -0,0 +1,8 @@
+import 'package:practwork1/practwork1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
