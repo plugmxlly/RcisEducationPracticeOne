3913438f0625edd34696465e6909fa659e7cf47b
plugmxlly <117053595+plugmxlly@users.noreply.github.com>
2022-11-03 14:54:30 +0700

    Add files via upload

---
+++ практ 1.4.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+
+// Задание 1
+
+//   int n = int.parse(stdin.readLineSync()!);
+//   int a = 1;
+//   for (int i = 3; i <= n; i += 3) {
+//     a *= i;
+//   }
+//   print(a);
+// 
+
+// Задание 5
+
+   // print("Введите X");
+  // int x = int.parse(stdin.readLineSync()!);
+  // print("Введите Y");
+  // int y = int.parse(stdin.readLineSync()!);
+
+  // if (x > -1 && x < 3) {
+  //   if (y > -2 && y < 4) {
+  //     print("Точка принадлежит координатам a, b");
+  //   } else {
+  //     print("Точка не принадлежит координатам a, b");
+  //   }
+  // } else {
+  //   print("Точка не принадлежит координатам a, b");
+  // }
+}
